7c61ef9665cf6c1a893a8cad6c00970f2a9fe65f
lingluojun <l247677858my@126.com>
2018-02-01 21:24:06 +0800

    增加相应注释

--- src/main/java/com/six/dove/common/AbstractService.java
+++ src/main/java/com/six/dove/common/AbstractService.java
@@ -7,6 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * 服务调用的基本单元
+ *
+ * @author yangshuang
  * @author sixliu
  * @date 2018年1月17日
  * @email 359852326@qq.com
@@ -14,52 +17,69 @@ import org.slf4j.LoggerFactory;
  */
 public abstract class AbstractService implements Service {
 
-	private static Logger log = LoggerFactory.getLogger(AbstractService.class);
+    private static Logger log = LoggerFactory.getLogger(AbstractService.class);
 
-	private static AtomicReferenceFieldUpdater<AbstractService, State> STATE_UPDATE = AtomicReferenceFieldUpdater
-			.newUpdater(AbstractService.class, State.class, "state");
-	private final String name;
-	private volatile State state = State.INIT;
+    /**
+     * 服务状态更新
+     */
+    private static AtomicReferenceFieldUpdater<AbstractService, State> STATE_UPDATE = AtomicReferenceFieldUpdater
+            .newUpdater(AbstractService.class, State.class, "state");
+    /**
+     * 服务名称
+     */
+    private final String name;
 
-	public AbstractService(String name) {
-		Objects.requireNonNull(name);
-		this.name = name;
-	}
+    /**
+     * 服务状态
+     */
+    private volatile State state = State.INIT;
 
-	@Override
-	public final String getName() {
-		return name;
-	}
+    public AbstractService(String name) {
+        Objects.requireNonNull(name);
+        this.name = name;
+    }
 
-	@Override
-	public final State getState() {
-		return state;
-	}
+    @Override
+    public final String getName() {
+        return name;
+    }
 
-	@Override
-	public final boolean isRunning() {
-		return State.START == state;
-	}
+    @Override
+    public final State getState() {
+        return state;
+    }
 
-	@Override
-	public final void start() {
-		if (STATE_UPDATE.compareAndSet(this, State.INIT, State.START)) {
-			log.info("will start service[" + getName() + "]");
-			doStart();
-			log.info("started service[" + getName() + "]");
-		}
-	}
+    @Override
+    public final boolean isRunning() {
+        return State.START == state;
+    }
 
-	protected abstract void doStart();
+    @Override
+    public final void start() {
+        if (STATE_UPDATE.compareAndSet(this, State.INIT, State.START)) {
+            log.info("will start service[" + getName() + "]");
+            doStart();
+            log.info("started service[" + getName() + "]");
+        }
+    }
 
-	@Override
-	public final void stop() {
-		if (STATE_UPDATE.compareAndSet(this, State.START, State.STOP)) {
-			log.info("will stop service[" + getName() + "]");
-			doStop();
-			log.info("stoped service[" + getName() + "]");
-		}
-	}
 
-	protected abstract void doStop();
+    /**
+     * 启用服务
+     */
+    protected abstract void doStart();
+
+    @Override
+    public final void stop() {
+        if (STATE_UPDATE.compareAndSet(this, State.START, State.STOP)) {
+            log.info("will stop service[" + getName() + "]");
+            doStop();
+            log.info("stoped service[" + getName() + "]");
+        }
+    }
+
+    /**
+     * 停止无法
+     */
+    protected abstract void doStop();
 }
